e907242d54479fbd73db34a00a4cc2996d219688
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:40:02 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "azurerm_windows_web_app" "windows_web_apps" {
         action      = ip_restriction.value.action
         description = ip_restriction.value.description
         dynamic "headers" {
-          for_each = ip_restriction.value.headers != null ? [ip_restriction.value.headers] : []
+          for_each = ip_restriction.value.headers != null ? ip_restriction.value.headers : []
           content {
             x_azure_fdid      = headers.value.x_azure_fdid
             x_fd_health_probe = headers.value.x_fd_health_probe
@@ -158,7 +158,7 @@ resource "azurerm_windows_web_app" "windows_web_apps" {
         action      = scm_ip_restriction.value.action
         description = scm_ip_restriction.value.description
         dynamic "headers" {
-          for_each = scm_ip_restriction.value.headers != null ? [scm_ip_restriction.value.headers] : []
+          for_each = scm_ip_restriction.value.headers != null ? scm_ip_restriction.value.headers : []
           content {
             x_azure_fdid      = headers.value.x_azure_fdid
             x_fd_health_probe = headers.value.x_fd_health_probe
